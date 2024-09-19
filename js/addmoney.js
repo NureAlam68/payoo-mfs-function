be4f1cee659b531650de0b76357cebd9ebbd56a8
NureAlam68 <nurealam151068@gmail.com>
2024-09-19 23:36:38 +0600

    home section done

---
+++ js/addmoney.js
@@ -0,0 +1,10 @@
+// dry- code repeat lom korar jonno function use hobe.... 
+
+
+document.getElementById('btn-add-money').addEventListener('click', function(event) {
+    //prevent page reload stop after submit form
+    event.preventDefault();
+
+    
+
+});
