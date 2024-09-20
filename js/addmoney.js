0c60742f71bef97d1f4ddd6b2b08bd17e1efda42
NureAlam68 <nurealam151068@gmail.com>
2024-09-20 11:53:19 +0600

    add money js done

--- js/addmoney.js
+++ js/addmoney.js
@@ -5,6 +5,17 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     //prevent page reload stop after submit form
     event.preventDefault();
 
-    
+    const addMoney = getInputFieldValueById('input-add-money');
+    const pinNumber = getInputFieldValueById('input-pin-number');
+
+    // wrong way not for serious website
+    if(pinNumber === 1234) {
+        const accountBalance = getTextFieldValueById('account-balance');
+        const newBalance = accountBalance + addMoney;
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else {
+        alert('Failed to add money! Try again..')
+    }
 
 });
